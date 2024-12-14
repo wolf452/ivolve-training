c075148a095629d250ea96cc93a4a38357aaad57
ahmed maher <121304760+wolf8534@users.noreply.github.com>
2024-12-14 18:21:47 +0200

    Update kubernetesLib.groovy

--- jenkins/lab2/var/kubernetesLib.groovy
+++ jenkins/lab2/var/kubernetesLib.groovy
@@ -1,17 +1,5 @@
-def applyManifest(String kubeConfigPath, String token, String yamlFile, String namespace) {
+def deployToKubernetes(String kubeConfigPath, String token, String yamlFile, String namespace) {
     sh """
     kubectl --kubeconfig=${kubeConfigPath} --token=${token} apply -f ${yamlFile} -n ${namespace}
-    """
-}
-
-def deleteDeployment(String kubeConfigPath, String token, String deploymentName, String namespace) {
-    sh """
-    kubectl --kubeconfig=${kubeConfigPath} --token=${token} delete deployment ${deploymentName} -n ${namespace} || true
-    """
-}
-
-def checkDeploymentStatus(String kubeConfigPath, String token, String deploymentName, String namespace) {
-    sh """
-    kubectl --kubeconfig=${kubeConfigPath} --token=${token} rollout status deployment/${deploymentName} -n ${namespace}
     """
 }
