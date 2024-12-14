92bdc867c71fa3e84f0b2d581d75253e7ecb916b
ahmed maher <121304760+wolf8534@users.noreply.github.com>
2024-12-14 19:36:57 +0200

    Delete jenkins/lab2/var directory

--- jenkins/lab2/var/gitLib.groovy
+++
@@ -1,9 +0,0 @@
-def checkoutRepository(String repoUrl, String branch = 'main', String subDir = '') {
-    if (subDir) {
-        dir(subDir) {
-            git url: repoUrl, branch: branch
-        }
-    } else {
-        git url: repoUrl, branch: branch
-    }
-}
