59e51ba193cd4c520f094c6c2699d7a3dc169df3
ahmed maher <121304760+wolf8534@users.noreply.github.com>
2024-12-14 19:44:06 +0200

    Delete jenkins/lab2/vars directory

--- jenkins/lab2/vars/deploymentLib.groovy
+++
@@ -1,3 +0,0 @@
-def editDeploymentYaml(String yamlFile, String dockerImage) {
-    sh "sed -i 's|image:.*|image: ${dockerImage}|g' ${yamlFile}"
-}
--- jenkins/lab2/vars/kubernetesLib.groovy
+++
@@ -1,5 +0,0 @@
-def deployToKubernetes(String kubeConfigPath, String token, String yamlFile, String namespace) {
-    sh """
-    kubectl --kubeconfig=${kubeConfigPath} --token=${token} apply -f ${yamlFile} -n ${namespace}
-    """
-}
